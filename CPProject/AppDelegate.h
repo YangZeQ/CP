3020d8f1723b77adf32f03a9e3076aee6decd865
YangZeQ <982002170@qq.com>
2018-07-23 09:14:15 +0800

    Initial Commit

---
+++ CPProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CPProject
+//
+//  Created by yang on 2018/7/23.
+//  Copyright © 2018年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CPProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CPProject
+//
+//  Created by yang on 2018/7/23.
+//  Copyright © 2018年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
